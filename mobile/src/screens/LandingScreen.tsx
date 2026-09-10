84f81f2334dde7f4ef11d41c9e599eb1244970eb
keshav kandoi <kandoikeshav2004@gmail.com>
2026-09-10 19:42:30 +0530

    Add push token registration in LandingScreen component

--- mobile/src/screens/LandingScreen.tsx
+++ mobile/src/screens/LandingScreen.tsx
@@ -15,6 +15,7 @@ import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import { MaterialIcons, Feather } from '@expo/vector-icons'
 import { LinearGradient } from 'expo-linear-gradient'
+import { registerPushToken } from '../lib/push'
 
 const { width } = Dimensions.get('window')
 const BLUE = '#4E7FFF'
@@ -120,6 +121,7 @@ export default function LandingScreen() {
   const pulseAnim = useRef(new Animated.Value(1)).current
 
   useEffect(() => {
+    void registerPushToken().catch(() => {})
     const cachedAvatar = getUserScopedCacheSync<{ photoUrl?: string; initials: string }>('avatar_profile_cache')
     if (cachedAvatar) setAvatarProfile(cachedAvatar)
     const cachedPlaces = getUserScopedCacheSync<any[]>('landing_nearby_places')
